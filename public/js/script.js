681c439596361d638c8e3c3c8221436cb378e6fd
farah2003 <farah.shaqoura@gmail.com>
2022-02-16 01:35:06 +0200

    add file relates 7-handle-Home

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,18 @@
-console.log('i AM IN CJS FILE')
-const input =document.getElementById("input");
-const getData=()=>{
+console.log("i AM IN CJS FILE");
+const input = document.getElementById("input");
+const getData = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    console.log(xhr.readyState);
 
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        console.log(xhr.readyState)
-      
-        if (xhr.status === 200) {
-            if (xhr.readyState === 4) {
-                console.log(xhr.responseText)
-                const data = JSON.parse(xhr.responseText);
-             
-
-            }
-        }
+    if (xhr.status === 200) {
+      if (xhr.readyState === 4) {
+        console.log(xhr.responseText);
+        const data = JSON.parse(xhr.responseText);
+      }
     }
-    xhr.open('GET', '/search', true);
-    xhr.send();
-}
-input.addEventListener('input',getData);
+  };
+  xhr.open("GET", "/search", true);
+  xhr.send();
+};
+input.addEventListener("input", getData);
